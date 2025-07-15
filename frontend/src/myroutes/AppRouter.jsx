b645faefb559f0f435a8f519f47fe353475c5078
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-15 20:38:51 +0530

    added codes here updates structure

--- frontend/src/myroutes/AppRouter.jsx
+++ frontend/src/myroutes/AppRouter.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 // import pages
+import Signup from '../auth/signup/Signup'
+import Login from '../auth/login/Login'
 import Profile from '../features/profile/Profile'
 import Feeds from '../features/feeds/Feeds'
-import Login from '../auth/login/login'
-import Signup from '../auth/signup/Signup'
-
-export default function AppRouter() {
+function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/feeds" element={<Feeds />} />
-      <Route path="/signup" element={<Signup />} />
     </Routes>
   )
 }
+
+export default AppRouter
