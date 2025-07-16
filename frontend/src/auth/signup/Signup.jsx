26f110c2182bbc19c2dcfd2b759631bc8ab1486b
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-17 00:43:50 +0530

    signup layout text now working.

--- frontend/src/auth/signup/Signup.jsx
+++ frontend/src/auth/signup/Signup.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
+import './signup.css';
 
 const images = [
     'https://images.pexels.com/photos/3184435/pexels-photo-3184435.jpeg',
@@ -27,6 +28,12 @@ function ImageSlider({ images }) {
             <img src={images[currentIndex]}
                 alt={`Slide ${currentIndex + 1}`} className='h-[20vh] md:h-[100vh] md:object-cover md:w-[100%]'
             />
+            <div className='absolute top-0 background-style-left-container w-1/2 left-0 transform  text-white text-2xl md:text-6xl font-bold h-[20vh] md:h-[100vh]'>
+                <span className='text-3xl md:text-5xl m-4 text-center flex justify-center gap-4 items-center'>Chichat <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-20" style={{color: "#7257ff"}}>
+                    <path fill-rule="evenodd" d="M4.848 2.771A49.144 49.144 0 0 1 12 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97-1.94.284-3.916.455-5.922.505a.39.39 0 0 0-.266.112L8.78 21.53A.75.75 0 0 1 7.5 21v-3.955a48.842 48.842 0 0 1-2.652-.316c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97Z" clip-rule="evenodd" />
+                </svg>
+                </span>
+            </div>
         </div>
     )
 }
@@ -34,7 +41,6 @@ function Signup() {
     return (
         <>
             <div className='min-h-screen flex flex-col md:flex-row  items-center justify-start'>
-
                 <div className='hidden md:block w-1/2'>
                     <motion.div
                         initial={{ opacity: 0 }}
