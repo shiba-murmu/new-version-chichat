587c9deadc0da131c698f29eb0b91487f14402df
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-17 19:36:25 +0530

    password visibility in login page.

--- frontend/src/auth/login/Login.jsx
+++ frontend/src/auth/login/Login.jsx
@@ -28,6 +28,11 @@ function LockIcon() {
 function Login() {
     const [lock, setLock] = useState(true);
     const [unlock, setUnlock] = useState(false);
+    const [isEyeOpened, setIsEyeOpened] = useState(false); // for toggle eye
+    const toggleEye = () => {
+        // for password
+        setIsEyeOpened(!isEyeOpened);
+    }
     useEffect(() => {
         const interval = setInterval(() => {
             setLock((prev) => !prev);
@@ -86,7 +91,19 @@ function Login() {
                             </div>
                             <form action="" className='flex flex-col justify-center items-center md:justify-center md:items-center gap-4 w-60'>
                                 <input type="text" placeholder='Email or username' className='border input-style md:w-80   text-sm md:text-md border-[#7257ff] rounded w-[16rem] p-2.5 focus:outline-[#7257ff]' />
-                                <input type="password" placeholder='Password' className='rounded md:w-80  text-sm input-style  border-[#7257ff] focus:outline-[#7257ff] w-[16rem] border p-2.5 ' />
+                                <div className='relative w-fit md:w-80'>
+                                    <input type={isEyeOpened ? 'text' : 'password'} placeholder='Password' className='rounded md:w-80  text-sm input-style  border-[#7257ff] focus:outline-[#7257ff] w-[16rem] border p-2.5 ' />
+                                    <div className='absolute inset-y-0 right-3 flex items-center'>
+                                        {
+                                            isEyeOpened ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" className="size-5 md:size-6" style={{ color: '#7257ff' }} onClick={toggleEye}>
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                            </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" class="size-5 md:size-6" style={{ color: '#7257ff' }} onClick={toggleEye}>
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                                            </svg>
+                                        }
+                                    </div>
+                                </div>
                                 <Link to={'/profile'}>
                                     <button type='submit' className='custom-background hover:bg-blue-600 md:w-80 text-sm md:text-md hover:cursor-pointer w-[16rem] text-white p-2.5 rounded '>Sign in</button>
 
