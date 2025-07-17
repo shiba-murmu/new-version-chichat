d9e8036b6cc75c59c9a53307017ab286d6be627d
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-18 00:55:56 +0530

    searching profile also added its notification bar.

--- frontend/src/features/Search/ProfileSearching.jsx
+++ frontend/src/features/Search/ProfileSearching.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+function ProfileNavbar() {
+    return(
+        <>
+            <div className='bg-gray-50 h-12 border-t-0 border border-b-gray-200 flex items-center'>
+                <input type="text" placeholder='Search' className='bg-gray-50 mx-3 border w-full border-gray-300 rounded-full px-3 py-1' />
+            </div>
+        </>
+    )
+}
 function ProfileSearching() {
   return (
-    <div>ProfileSearching</div>
+    // <div>ProfileSearching</div>
+    <>
+        <ProfileNavbar />
+        <div>No profile found!</div>
+    </>
   )
 }
 
