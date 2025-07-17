7431a56b3c057d325c1f7dc2c995f1a3697e6fec
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-17 23:43:18 +0530

    Content added in the model.

--- frontend/src/features/profile/Profile.jsx
+++ frontend/src/features/profile/Profile.jsx
@@ -23,7 +23,7 @@ function Sidebar({ onClose }) {
             <div className='absolute top-0 left-0 w-full h-full sidebar-overlay-background'>
                 {/* Sidebar/Menu Container */}
                 <div ref={SidebarRef} className="fixed rounded-l-4xl right-0 top-0 w-64 h-full bg-white shadow-lg  z-60 animate-slide-in">
-                    <div className='flex justify-end h-12 md:h-14 p-2 bg-[#e0dfe4] rounded-tl-4xl border-gray-200 text-sm'>
+                    <div className='flex justify-end h-12 md:h-14 p-2 bg-[#f2f2f2] rounded-tl-4xl border-gray-200 text-sm'>
                         <button onClick={onClose}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  width="24" height="24" fill="currentColor" className="size-7 md:size-9">
                                 <path fillRule="evenodd" strokeWidth={2} d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
@@ -31,10 +31,10 @@ function Sidebar({ onClose }) {
                         </button>
                     </div>
                     <ul className="space-y-2">
-                        <li className="hover:text-blue-500 cursor-pointer">Home</li>
-                        <li className="hover:text-blue-500 cursor-pointer">Profile</li>
-                        <li className="hover:text-blue-500 cursor-pointer">Settings</li>
-                        <li className="hover:text-blue-500 cursor-pointer">Logout</li>
+                        <li className="hover:text-blue-500 pl-4 border-b border-gray-200 rounded-l-2xl text-sm md:text-md py-3 cursor-pointer">Home</li>
+                        <li className="hover:text-blue-500 pl-4 border-b border-gray-200 rounded-l-2xl text-sm md:text-md py-3 cursor-pointer">Profile</li>
+                        <li className="hover:text-blue-500 pl-4 border-b border-gray-200 rounded-l-2xl text-sm md:text-md py-3 cursor-pointer">Settings</li>
+                        <li className="hover:text-blue-500 pl-4 border-b border-gray-200 rounded-l-2xl text-sm md:text-md py-3 cursor-pointer">Logout</li>
                     </ul>
 
                 </div>
