0b154e73bd64be17cf215b5afee4dd2d8dd23a8f
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-18 09:37:44 +0530

    removed code/

--- frontend/src/features/Followers/Followers_profile/Followers_Profile.jsx
+++ frontend/src/features/Followers/Followers_profile/Followers_Profile.jsx
@@ -13,7 +13,6 @@ function Followers_Profile() {
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="size-7 md:size-9">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                             </svg>
-                            id : {id}
                         </button>
                         
                     </div>
