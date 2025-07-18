644d924fa8d627d675a57812b731812e4bb5b36c
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-18 16:39:35 +0530

    reverse the desktop design.

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,18 +5,9 @@ function Footer() {
         <div className='fixed bottom-0 left-0 md:bottom-0 md:left-0 md:right-0 right-0'>
             <div className='flex justify-around md:justify-around mx-auto md:w-[50vw]  shadow-md shadow-gray-400 items-center py-5 md:p-5 bg-[#e0dfe4] border-t border-gray-100'>
                 
-                <section className='md:hidden'>
+                <section>
                     {/* For home mobile version */}
                     <Link to={'/home'}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="size-7">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                        </svg>
-                    </Link>
-                </section>
-
-                <section className='hidden md:block'>
-                    {/* For home for desktop */}
-                    <Link to={'/desktop-home'}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="size-7">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                         </svg>
--- frontend/src/pages/Home/desktopVersionHome/DesktopHome.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DesktopHome() {
-  return (
-    <div>DesktopHome</div>
-  )
-}
-
-export default DesktopHome
