bb585dfd59b2d0fe091299ba50d78543a8320881
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-21 20:56:17 +0530

    added hover effect in the delete icons

--- frontend/src/features/Notifications/Notification.jsx
+++ frontend/src/features/Notifications/Notification.jsx
@@ -54,7 +54,7 @@ function UserNotification() {
                 </div>
                 <div className='px-3 py-2 flex justify-end items-center'>
                     {/* This div is for delete the messages from the notification */}
-                    <button onClick={toggleMenu}>
+                    <button onClick={toggleMenu} className='hover:cursor-pointer'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1" stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                         </svg>
@@ -70,8 +70,8 @@ function UserNotification() {
                         <div ref={notificationRef} className='border w-64 border-[#c5c5c5] h-30 rounded-2xl flex justify-center items-center p-3 gap-3 flex-col'>
                             <p className='text-sm text-center'>Are you sure want to delete the notification ?</p>
                             <div>
-                                <button className='mx-2 bg-[#7257ff] text-sm rounded-full w-20 py-1 text-white' onClick={handleDelete}>delete</button>
-                                <button className='mx-2 bg-[#9b9b9b] text-sm rounded-full w-20 py-1 text-white' onClick={toggleMenu}>No</button>
+                                <button className='mx-2 bg-[#7257ff] text-sm rounded-full w-20 py-1 text-white hover:cursor-pointer' onClick={handleDelete}>delete</button>
+                                <button className='mx-2 bg-[#9b9b9b] text-sm rounded-full w-20 py-1 text-white hover:cursor-pointer' onClick={toggleMenu}>No</button>
                             </div>
                         </div>
                     </div>
