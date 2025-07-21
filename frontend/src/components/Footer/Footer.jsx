cbc550c9fa9610b5c6f2510b32f9e3e0a02fb201
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-21 18:46:22 +0530

    Trying to design the UI to reponsive for desktop also

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,43 +1,50 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 function Footer() {
     return (
-        <div className='fixed bottom-0 left-0 md:bottom-0 md:left-0 md:right-0 right-0'>
-            <div className='flex justify-around md:justify-around mx-auto md:w-[50vw]  shadow-md shadow-gray-400 items-center py-5 md:p-5 bg-[#e0dfe4] border-t border-gray-100'>
+        <div className='fixed bottom-0 left-0 right-0 z-50 bg-[#e0dfe4] border-t border-gray-200 shadow-md'>
+            <div className='flex justify-around items-center py-4 md:py-5 md:w-[50vw] mx-auto'>
+
+                {/* Home */}
                 <section>
-                    {/* For home mobile version */}
                     <Link to={'/home'}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="size-7">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="size-7" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                         </svg>
                     </Link>
                 </section>
 
+                {/* Search */}
                 <section>
-                    {/* For search */}
                     <Link to={'/search'}>
-
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-7">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="size-7" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                         </svg>
                     </Link>
                 </section>
+
+                {/* Add Post */}
                 <section>
-                    {/* For posting photos */}
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-7">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="size-7" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
                 </section>
+
+                {/* Profile */}
                 <section>
-                    {/* For profile */}
                     <Link to={'/profile'}>
-                        <img src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt=""
-                            className='h-7 w-7 rounded-full object-cover' />
+                        <img
+                            src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                            className='h-7 w-7 rounded-full object-cover'
+                            alt="Profile"
+                        />
                     </Link>
                 </section>
+
             </div>
         </div>
     )
 }
 
-export default Footer
+export default Footer;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -23,14 +23,14 @@ function PostingCard() {
                         <span className='text-xs text-gray-500'>johndoe</span>
                     </section>
                     <section className='ml-auto'>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
                         </svg>
                     </section>
                 </div>
                 <div>
                     <img src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                        className='w-full h-96 object-cover' alt="" />
+                        className='w-full h-80 md:h-[25rem] md:w-[100%] object-cover' alt="" />
                 </div>
                 <div>
                     <p className='px-2 py-1 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, quod.</p>
@@ -45,14 +45,20 @@ function Home() {
 
     return (
         <>
-            <div className='max-h-[40.4rem] md:max-h-[30.5rem]
-             md:w-full overflow-scroll'>
-                    <Navbar />
+            <Navbar />
+
+            <div className="flex flex-col min-h-screen bg-amber-100">
+                <div className="flex-grow overflow-y-auto px-2 py-4 md:w-[40rem] md:mx-auto custom-scrollbar pb-20">
+                    {/* Add enough bottom padding to prevent hiding behind footer */}
                     <PostingCard />
                     <PostingCard />
                     <PostingCard />
                     <PostingCard />
+                    <PostingCard />
+                </div>
             </div>
+
+            
         </>
     )
 }
