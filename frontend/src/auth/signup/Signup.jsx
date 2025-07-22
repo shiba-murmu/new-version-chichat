0cc28bf175469a62db7d1e227c01061703292667
Shiba Murmu <shibamurmu001@gmail.com>
2025-07-23 05:16:38 +0530

    email validation applied for already saved in the database is available or not also username validations is already available or not in the database.

--- frontend/src/auth/signup/Signup.jsx
+++ frontend/src/auth/signup/Signup.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 import './signup.css';
+import validator from 'validator';
+
 
 const images = [
     'https://images.pexels.com/photos/3184435/pexels-photo-3184435.jpeg',
@@ -40,6 +42,17 @@ function ImageSlider({ images }) {
 function Signup() {
     const [isEyeOpened, setIsEyeOpened] = useState(false); // for toggle eye
     const [isEyeOpenedConfirmPassword, setIsEyeOpenedConfirmPassword] = useState(false);
+
+    ///////////////Email validator //////////////
+    const [isEmailValid, setIsEmailValid] = useState(false);
+    const [emailError, setEmailError] = useState('');
+
+    // /////////// username validator ///////////
+    const [isUsernameValid, setIsUsernameValid] = useState(false);
+    const [usernameError, setUsernameError] = useState('');
+
+
+
     const toggleEyeConfirmPassword = () => {
         // for password confirm password ....
         setIsEyeOpenedConfirmPassword(!isEyeOpenedConfirmPassword);
@@ -47,6 +60,81 @@ function Signup() {
     const toggleEye = () => {
         // for password
         setIsEyeOpened(!isEyeOpened);
+    }
+
+    const [formData, setFormData] = useState({
+        first_name: '',
+        last_name: '',
+        username: '',
+        email: '',
+        password: '',
+        confirm_password: '',
+    })
+
+    // for email validations.
+    useEffect(() => {
+        if (formData.email == '') {
+            setEmailError('Enter valid email address');
+            setIsEmailValid(false);
+        } else if (!validator.isEmail(formData.email)) {
+            setEmailError('Invalid email address');
+            setIsEmailValid(false);
+        } else if (!formData.email.endsWith('@gmail.com')) {
+            setEmailError('Only Gmail emails are allowed');
+            setIsEmailValid(false);
+        } else {
+            setEmailError('Email is valid');
+            // Here one api will send to the backend to check the email is already exist or not
+            setIsEmailValid(true);
+        }
+    }, [formData.email])
+
+    useEffect(() => {
+        if (formData.username == '') {
+            setUsernameError('Enter valid username');
+            setIsUsernameValid(false);
+        } else if (formData.username.length <= 4) {
+            setUsernameError('Invalid username');
+            setIsUsernameValid(false);
+        } else if (formData.username.length >= 15) {
+            setUsernameError('Invalid username');
+            setIsUsernameValid(false);
+        } else if (!/^[a-z0-9_]+$/.test(formData.username)) {
+            setUsernameError('Invalid username');
+            setIsUsernameValid(false);
+        } else if (/^[0-9]/.test(formData.username)) {
+            setUsernameError('Invalid username');
+            setIsUsernameValid(false);
+        } else if (/\s/.test(formData.username)) {
+            setEmailError('Invalid username');
+            setIsUsernameValid(false);
+        } else {
+            setUsernameError('Username is valid');
+            // Here one api will send to the backend to check the username is already exist or not
+            setIsUsernameValid(true);
+        }
+    }, [formData.username])
+
+
+    // Update form values
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+
+        // some other code 
+        try {
+            console.log(formData)
+        }
+        catch (error) {
+            console.log(error)
+        }
     }
     return (
         <>
@@ -68,31 +156,54 @@ function Signup() {
                         <span className='text-sm md:text-md text-center'>Welcome! Let’s get you set up. <br />It only takes a minute to get started.</span>
                     </div>
                     <div className='flex flex-col items-center'>
-                        <form action="" className='flex flex-col gap-4 px-10 mb-3'>
-                            <input type="text" placeholder='First name' className='border  md:w-80  text-sm md:text-md border-[#7257ff] w-[16rem] rounded p-2.5 focus:outline-[#7257ff]' />
-                            <input type="text" placeholder='Last name' className='border  md:w-80  text-sm md:text-md border-[#7257ff] w-[16rem] rounded p-2.5 focus:outline-[#7257ff]' />
+                        <form action="" onSubmit={handleSubmit} className='flex flex-col gap-4 px-10 mb-3'>
+                            <input type="text" name='first_name'
+                                onChange={handleChange}
+                                value={formData.first_name}
+                                placeholder='First name' className='border  md:w-80 required  text-sm md:text-md border-[#7257ff] w-[16rem] rounded p-2.5 focus:outline-[#7257ff]' />
+                            <input type="text" name='last_name'
+                                onChange={handleChange}
+                                value={formData.last_name}
+                                placeholder='Last name' className='border required  md:w-80  text-sm md:text-md border-[#7257ff] w-[16rem] rounded p-2.5 focus:outline-[#7257ff]' />
                             <div className='flex flex-col gap-0.5'>
-                                <input type="text" placeholder='Username' className='border  md:w-80  text-sm md:text-md border-[#7257ff] rounded p-2.5 focus:outline-[#7257ff]' />
-                                <label htmlFor="" className='text-xs text-[#7257ff] flex gap-1 items-center'>Username available<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-3">
-                                    <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
-                                </svg>
-                                </label>
+                                <input type="text" placeholder='Username' name='username'
+                                    onChange={handleChange}
+                                    value={formData.username}
+                                    className='border required  md:w-80  text-sm md:text-md border-[#7257ff] rounded p-2.5 focus:outline-[#7257ff]' />
+                                {/* Here username validations will be done. Where username is already exist or not */}
+                                {
+                                    isUsernameValid ? <label htmlFor="" className='text-xs text-[#7257ff] flex gap-1 items-center'>{usernameError}
+                                    </label> : <label htmlFor="" className='text-xs text-[#ff0000] flex gap-1 items-center'>{usernameError}
+                                    </label>
+                                }
+
+
                             </div>
                             <div className='flex flex-col gap-0.5'>
-                                <input type="email" placeholder='Email address' className='border  md:w-80  text-sm md:text-md border-[#7257ff] rounded p-2.5 focus:outline-[#7257ff]' />
-                                <label htmlFor="" className='text-xs text-[#7257ff] flex gap-1 items-center'>Email available<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-3">
-                                    <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
-                                </svg>
-                                </label>
+                                <input type="email" name='email'
+                                    onChange={handleChange}
+                                    value={formData.email}
+                                    placeholder='Email address'
+                                    className='border required  md:w-80  text-sm md:text-md border-[#7257ff] rounded p-2.5 focus:outline-[#7257ff]' />
+                                {
+                                    isEmailValid ? <label htmlFor="" className='text-xs text-[#7257ff] flex gap-1 items-center'>{emailError}
+                                    </label> : <label htmlFor="" className='text-xs text-red-500 flex gap-1 items-center'>{emailError}
+                                    </label>
+                                }
+
                             </div>
                             <div className='relative w-fit md:w-80'>
-                                <input type={isEyeOpened ? 'text' : 'password'} placeholder='Password' className='rounded md:w-80  text-sm input-style  border-[#7257ff] focus:outline-[#7257ff] w-[16rem] border p-2.5 ' />
+                                <input type={isEyeOpened ? 'text' : 'password'}
+                                    name='password'
+                                    onChange={handleChange}
+                                    value={formData.password}
+                                    placeholder='Password' className='rounded required md:w-80  text-sm input-style  border-[#7257ff] focus:outline-[#7257ff] w-[16rem] border p-2.5 ' />
                                 <div className='absolute inset-y-0 right-3 flex items-center'>
                                     {
                                         isEyeOpened ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" className="size-5 md:size-6 hover:cursor-pointer" style={{ color: '#7257ff' }} onClick={toggleEye}>
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                        </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" class="size-5 md:size-6 hover:cursor-pointer" style={{ color: '#7257ff' }} onClick={toggleEye}>
+                                        </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" className="size-5 md:size-6 hover:cursor-pointer" style={{ color: '#7257ff' }} onClick={toggleEye}>
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
                                         </svg>
                                     }
@@ -100,13 +211,15 @@ function Signup() {
                             </div>
 
                             <div className='relative w-fit md:w-80'>
-                                <input type={isEyeOpenedConfirmPassword ? 'text' : 'password'} placeholder='Confirm Password' className='rounded md:w-80  text-sm input-style  border-[#7257ff] focus:outline-[#7257ff] w-[16rem] border p-2.5 ' />
+                                <input type={isEyeOpenedConfirmPassword ? 'text' : 'password'}
+                                    name='confirm_password' onChange={handleChange} value={formData.confirm_password}
+                                    placeholder='Confirm Password' className=' required rounded md:w-80  text-sm input-style  border-[#7257ff] focus:outline-[#7257ff] w-[16rem] border p-2.5 ' />
                                 <div className='absolute inset-y-0 right-3 flex items-center'>
                                     {
                                         isEyeOpenedConfirmPassword ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" className="size-5 md:size-6 hover:cursor-pointer" style={{ color: '#7257ff' }} onClick={toggleEyeConfirmPassword}>
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                        </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" class="size-5 md:size-6 hover:cursor-pointer" style={{ color: '#7257ff' }} onClick={toggleEyeConfirmPassword}>
+                                        </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" className="size-5 md:size-6 hover:cursor-pointer" style={{ color: '#7257ff' }} onClick={toggleEyeConfirmPassword}>
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
                                         </svg>
                                     }
@@ -115,7 +228,7 @@ function Signup() {
                             {/* <input type="password" placeholder='Confirm password' className='border  md:w-80  text-sm md:text-md border-[#7257ff] rounded p-2.5 focus:outline-[#7257ff]' /> */}
 
 
-                            <button className='bg-[#7257ff] text-white hover:cursor-pointer text-sm md:text-md rounded p-2.5'>Sign up</button>
+                            <button type='submit' className='bg-[#7257ff] text-white hover:cursor-pointer text-sm md:text-md rounded p-2.5'>Sign up</button>
                             <Link to={'/login'} className='text-center'>
                                 <p className='text-sm md:text-md'>Already have an account? <span className='text-[#7257ff]'>Log in</span></p>
                             </Link>
@@ -128,4 +241,4 @@ function Signup() {
     )
 }
 
-export default Signup
+export default Signup;
