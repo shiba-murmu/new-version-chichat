00f0c77aceff49061cd0e15d421c984458458a10
Shiba Murmu <shibamurmu001@gmail.com>
2025-08-06 19:44:35 +0530

    jwt timing resfresh token

--- backend/chichatapp/settings.py
+++ backend/chichatapp/settings.py
@@ -59,8 +59,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     # set time for the token expiry
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -57,8 +57,6 @@ function Home() {
                     <PostingCard />
                 </div>
             </div>
-
-            
         </>
     )
 }
