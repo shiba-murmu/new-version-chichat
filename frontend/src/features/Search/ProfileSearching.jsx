97ed0bcc1bcefae1c77f819dd95d29b1e9e3e018
Shiba Murmu <shibamurmu001@gmail.com>
2025-08-06 15:17:29 +0530

    remove some code.

--- frontend/src/features/Search/ProfileSearching.jsx
+++ frontend/src/features/Search/ProfileSearching.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import api from '../../api/axiosInstance'
-import { NavLink } from 'react-router-dom'
 
 function BackToHome() {
     return (
