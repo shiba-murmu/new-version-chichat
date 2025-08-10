098132ee3ad1f090da64a52ef963422e93597359
Shiba Murmu <shibamurmu001@gmail.com>
2025-08-10 22:02:00 +0530

    makes Ui more stylish.

--- frontend/src/auth/password/Forgot_password.jsx
+++ frontend/src/auth/password/Forgot_password.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function Forgot_password() {
 
     const [isEyeOpenedPassword, setIsEyeOpenedPassword] = useState(false);
     const [isEyeOpenedConfirmPassword, setIsEyeOpenedConfirmPassword] = useState(false);
+    const [otpPopUp, setOtpPopUp] = useState(false);
 
-    const Otp_verify = () => {
+    const Otp_verify = ({ closeUi }) => {
+
         return (
             <>
-                <div className='flex absolute z-20 left-0 right-0 items-center justify-end flex-col h-[60vh]'>
+                <div className='flex absolute z-20 left-0 right-0 items-center justify-end flex-col h-[65vh]'>
                     <div className='flex flex-col gap-2 items-center justify-center w-[90%] rounded-lg bg-[#7257ff] h-80'>
                         <div>
                             <span className='text-2xl font-bold text-yellow-300'>Verify your OTP.
@@ -23,10 +26,15 @@ function Forgot_password() {
                         <div className='w-[80%]'>
                             <input type='password' id='password' name='password' required className='input-style border border-[#e1e1e1]  rounded-md text-white
                         px-3 py-2 w-full focus:outline-[#c8c8c8]' placeholder='Password' />
-
                         </div>
                         <div className='w-[80%]'>
-                            <button type='submit' className='w-full hover:cursor-pointer bg-[#fcfcfc] text-[#7257ff]  py-2 rounded-md'>                            Recovery password
+                            <button type='submit' className='w-full hover:cursor-pointer bg-[#fcfcfc] text-[#7257ff]  py-2 rounded-md'>
+                                Recovery password
+                            </button>
+                        </div>
+                        <div className='w-[80%]'>
+                            <button type='submit' onClick={closeUi} className='w-full hover:cursor-pointer bg-[#fcfcfc] text-[#7257ff]  py-2 rounded-md'>
+                                Cancel
                             </button>
                         </div>
                     </div>
@@ -58,6 +66,10 @@ function Forgot_password() {
         // for password confirm password ....
         setIsEyeOpenedConfirmPassword(!isEyeOpenedConfirmPassword);
     }
+    const closePopup = () => {
+        // setOtpPop(false);
+        setOtpPopUp(false);
+    }
 
     useEffect(() => {
 
@@ -65,26 +77,29 @@ function Forgot_password() {
 
     return (
         <>
-            <Otp_verify />
+            {
+                otpPopUp && <Otp_verify closeUi={closePopup} />
+            }
+            {/* <Otp_verify /> */}
             <div className='text-center p-2 bg-[#7257ff] text-white'>
                 <span className='text-xl font-SemiBold'>Password Recovery</span>
             </div>
             <div>
-                <form action="" onSubmit={handleSubmit} className='flex flex-col gap-2'>
-                    <div className='my-2 px-2'>
-                        <label htmlFor="email" className='text-md'>Enter registered email address</label>
+                <form action="" onSubmit={handleSubmit} className='flex flex-col gap-1'>
+                    <div className='my-2 px-4'>
+                        <label htmlFor="email" className='text-sm'>Enter registered email address</label>
                     </div>
-                    <div className='px-2'>
-                        <input type="email" id='email' onChange={handleChange} value={formData.email} name='email' required className='input-style border border-[#7257ff] rounded-md
+                    <div className='px-5'>
+                        <input type="email" id='email' onChange={handleChange} value={formData.email} name='email' required className='text-sm border border-[#7257ff] rounded-md
                         px-3 py-2 w-full focus:outline-[#7257ff]' placeholder='Email address' />
                     </div>
-                    <div className='my-2 px-2'>
-                        <label htmlFor="password" className='text-md'>Enter new password!</label>
+                    <div className='my-2 px-4'>
+                        <label htmlFor="password" className='text-sm'>Enter new password!</label>
                     </div>
-                    <div className=' relative w-full px-2'>
-                        <input type={isEyeOpenedPassword ? 'text' : 'password'} id='password' onChange={handleChange} value={formData.password} name='password' required className='input-style border border-[#7257ff] rounded-md
+                    <div className=' relative w-full px-5'>
+                        <input type={isEyeOpenedPassword ? 'text' : 'password'} id='password' onChange={handleChange} value={formData.password} name='password' required className='input-style border text-sm border-[#7257ff] rounded-md
                         px-3 py-2 w-full focus:outline-[#7257ff]' placeholder='Password' />
-                        <div className='absolute inset-y-0 right-5 flex items-center'>
+                        <div className='absolute inset-y-0 right-7 flex items-center'>
                             {
                                 isEyeOpenedPassword ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" className="size-5 md:size-6 hover:cursor-pointer" style={{ color: '#7257ff' }} onClick={toggleEyePassword}>
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
@@ -95,13 +110,13 @@ function Forgot_password() {
                             }
                         </div>
                     </div>
-                    <div className='my-2 px-2'>
-                        <label htmlFor="confirm-password" className='text-md'>Confirm password!</label>
+                    <div className='my-2 px-4'>
+                        <label htmlFor="confirm-password" className='text-sm'>Confirm password!</label>
                     </div>
-                    <div className=' relative w-full px-2'>
-                        <input type={`${isEyeOpenedConfirmPassword ? 'text' : 'password'}`} id='confirm-password' name='confirm_password' onChange={handleChange} value={formData.confirm_password} required className='input-style border border-[#7257ff] rounded-md
+                    <div className=' relative w-full px-5'>
+                        <input type={`${isEyeOpenedConfirmPassword ? 'text' : 'password'}`} id='confirm-password' name='confirm_password' onChange={handleChange} value={formData.confirm_password} required className='input-style text-sm border border-[#7257ff] rounded-md
                         px-3 py-2 w-full focus:outline-[#7257ff]' placeholder='Confirm password' />
-                        <div className='absolute inset-y-0 right-5 flex items-center'>
+                        <div className='absolute inset-y-0 right-7 flex items-center'>
                             {
                                 isEyeOpenedConfirmPassword ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" className="size-5 md:size-6 hover:cursor-pointer" style={{ color: '#7257ff' }} onClick={toggleEyeConfirmPassword}>
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
@@ -112,11 +127,15 @@ function Forgot_password() {
                             }
                         </div>
                     </div>
-                    <div className='px-2 my-2'>
-                        <button type='submit' className='w-full hover:cursor-pointer bg-[#7257ff] text-white py-2 rounded-md'>
-
+                    <div className='px-5 my-2'>
+                        <button type='submit' className='w-full text-sm hover:cursor-pointer bg-[#7257ff] text-white py-2 rounded-md'>
                             Recovery password
                         </button>
+                    </div>
+                    <div className='flex items-center justify-center text-sm'>
+                        <span>Already have an account?
+                            <Link to={'/login'} className='text-[#5d3dff] hover:text-[#7257ff]'> Sign in</Link>
+                        </span>
                     </div>
                 </form>
             </div>
